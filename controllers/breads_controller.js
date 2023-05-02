1e1ee1071b5c5a6a3ffa1e9e7ae3694c0c298c27
Chris Wehner <chrisw7128@gmail.com>
2023-05-02 15:12:55 -0700

    another day

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -14,12 +14,15 @@ breads.get("/new", (req, res) => {
   });
 });
 
-// INDEX
+// Index:
 breads.get("/", (req, res) => {
-  Bread.find().then(foundBreads => {
-    res.render("index", {
-      breads: foundBreads,
-      title: "Index Page",
+  Baker.find().then(foundBakers => {
+    Bread.find().then(foundBreads => {
+      res.render("index", {
+        breads: foundBreads,
+        bakers: foundBakers,
+        title: "Index Page",
+      });
     });
   });
 });
